bbe843c5190fb93ffa78fb51e9627d34d1f7340c
Matthew Bourque <bourque@stsci.edu>
2016-02-06 22:19:32 -0500

    Implemented equals method.

--- fraction/Driver.java
+++ fraction/Driver.java
@@ -6,10 +6,11 @@ class Driver {
     public static void main(String[] args) {
 
         Fraction fraction1 = new Fraction(1,3);
-        Fraction fraction2 = new Fraction(2,3);
+        Fraction fraction2 = new Fraction(4,12);
         System.out.println(fraction1);
         System.out.println(fraction2);
         System.out.println(fraction1.add(fraction2));
+        System.out.println(fraction1.equals(fraction2));
     }
 
 }
--- fraction/Fraction.java
+++ fraction/Fraction.java
@@ -73,8 +73,24 @@ class Fraction {
     }
 
 
-    public void equals(){
+    /**
+     * Check whether or not the given fraction is equal to this fraction
+     * @param frac - the fraction to reduce
+     * @return true if fraction is equal, false if it is not.
+     */
+    public Boolean equals(Fraction frac){
 
+        // First make sure the two fractions are in simplest form
+        Fraction thisFrac = new Fraction(this.numerator, this.denominator);
+        Fraction thisFracSimple = reduce(thisFrac);
+        Fraction fracSimple = reduce(frac);
+
+        // Check to see if they are equal
+        if (thisFracSimple.numerator == fracSimple.numerator && thisFracSimple.denominator == fracSimple.denominator) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
@@ -109,7 +125,7 @@ class Fraction {
 
 
     /**
-     * Override the toString method of the Object class
+     * Override the toString method
      */
     @Override
     public String toString() {
