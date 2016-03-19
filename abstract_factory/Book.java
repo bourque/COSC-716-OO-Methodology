fd0ce7e1b62b8471580b1aa0a41e5a7ab5d9de63
Matthew Bourque <bourque@stsci.edu>
2016-03-19 16:17:28 -0400

    Added exceptions to check for invalid parameters.

--- abstract_factory/Book.java
+++ abstract_factory/Book.java
@@ -5,6 +5,15 @@ public class Book {
     public String availability;
 
     public Book (String name, String type, String availability){
+
+        // Make sure the attributes are valid
+        if (!type.matches("fiction|non fiction")) {
+            throw new IllegalArgumentException("Type must be 'fiction' or 'non fiction'");
+        }
+        if (!availability.matches("ebook|physical|both")) {
+            throw new IllegalArgumentException("Availability must be 'ebook' or 'pysical'");
+        }
+
         this.name = name;
         this.type = type;
         this.availability = availability;
