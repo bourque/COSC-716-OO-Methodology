8abc575f3c07df43f8b8566c5a35d7585f7e2d0f
Matthew Bourque <bourque@stsci.edu>
2016-04-23 15:48:59 -0400

    Fixed aggregator.

--- restaurant/Aggregator.java
+++ restaurant/Aggregator.java
@@ -1,10 +1,18 @@
 public class Aggregator {
 
+    Menu menu;
+    Orders orders;
+
+    public Aggregator() {
+        menu = new Menu();
+        orders = new Orders();
+    }
+
     public Menu getMenu() {
-        return new Menu();
+        return menu;
     }
 
     public Orders getOrders() {
-        return new Orders();
+        return orders;
     }
 }
